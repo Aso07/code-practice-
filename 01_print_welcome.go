698f9c4a84c20123adc530fe166b8084b140447b
aso07 <agenesunday143@gmail.com>
2026-05-27 09:23:28 +0100

    01_print_welcome

---
+++ 01_print_welcome.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func PrintWelcome() {
+	fmt.Println("Welcome to Learn2Earn Nigeria Fellowship")
+}
+func main() {
+	PrintWelcome()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module CODE-PRACTICE
+
+go 1.22.2
